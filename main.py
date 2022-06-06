ef852dd7fc25c0a9881eed9a15f5a29726ff7f89
David Ilori <92074984+Ddluwole@users.noreply.github.com>
2022-06-06 17:43:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Check if a word is an anagrams 
+# Check if a word is an anagrams
 # Example:
 # find_anagrams("hello") --> False
 # find_anagrams("racecar") --> True
@@ -6,11 +6,12 @@
 
 def find_anagrams(word1, word2):
     # [assignment] Add your code here
-    if len(word1)!=len(word2):
-        return False
-    else: 
-        srt_word1 = sorted(word1)
-        srt_word2 = sorted (word2)
+    word1 = word1.replace(" ", "")
+    word2 = word2.replace(" ", "")
+    srt_word1 = sorted(word1)
+    srt_word2 = sorted(word2)
 
-        return srt_word1==srt_word2
+    return srt_word1 == srt_word2
 
+
+print(find_anagrams('silent', 'letn si'))
